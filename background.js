4d8b27ed13ac8739b598a894623a25b7659596bd
jx06T <50313tjx06@gmail.com>
2023-08-30 19:47:29 +0800

    modified:   background.js 	deleted:    content.css 	deleted:    content.js 	deleted:    images/Icon_128.png 	deleted:    images/Icon_16.png 	deleted:    images/Icon_32.png 	deleted:    images/Icon_48.png 	deleted:    images/Penguin.gif 	deleted:    images/Sharp.png 	deleted:    images/fish.png 	new file:   images/google.png 	new file:   images/google128.png 	new file:   images/google16.png 	new file:   images/google32.png 	new file:   images/google48.png 	deleted:    images/html.png 	deleted:    images/mouse.png 	deleted:    images/mouse2.png 	deleted:    images/pet_melt.gif 	deleted:    images/pet_rest.gif 	deleted:    images/pet_walk.gif 	modified:   manifest.json 	deleted:    popup.css 	deleted:    popup.html 	deleted:    popup.js

--- background.js
+++ background.js
@@ -1,62 +1,15 @@
-chrome.runtime.onInstalled.addListener(() => {
-    chrome.contextMenus.create({
-        id: "sampleContextMenu",
-        title: "Fish",
-        contexts: ["all"],
-        // type :"checkbox"
-    });
-});
-function randId() {
-    return Math.random().toString(36).substring(2.9) + Math.random().toString(36).substring(2.9)
-}
-function debug(...d) {
-    console.log(d);
-}
-chrome.contextMenus.onClicked.addListener((info, tab) => {
-    chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        function: debug,
-        args: [info]
-    });
-    if (info.menuItemId === "sampleContextMenu") {
-        chrome.tabs.sendMessage(tab.id, { greeting: "NewFish" });
-    }
-
-});
 chrome.commands.onCommand.addListener((command, tab) => {
-    if (command == "Sharp") {
-        chrome.tabs.sendMessage(tab.id, { greeting: "Sharp" });
-    } else if (command == "NewPet") {
-        const aPet = {
-            size: Math.floor(Math.random() * 260 + 40),
-            color: Math.floor(Math.random() * 360),
-            id: randId()
-        };
-
-        // 保存数据到chrome.storage
-        chrome.storage.local.get(["Pets"]).then((result) => {
-            let pets = result.Pets;
-            if (!pets) {
-                pets = []
+    if (command == "google") {
+        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+            let tab = tabs[0];
+            let key = tab.title
+            if (tab.url.includes('https://www.bing.com/search')) {
+                key = key.slice(0, key.length - 5)
+                chrome.tabs.create({ url: 'https://www.google.com.tw/search?q=' + key });
+            } else if (tab.url.includes('https://www.google.com.tw/search')){
+                key = key.slice(0, key.length -11)
+                chrome.tabs.create({ url: 'https://www.bing.com/search?q=' + key });
             }
-            pets.push(aPet)
-            chrome.storage.local.set({ Pets: pets }).then(() => {
-            });
-        });
-        chrome.tabs.sendMessage(tab.id, { greeting: "NewPet", data: aPet });
-    } else if (command == "isDeactivate") {
-        chrome.storage.local.get(["isDeactivate"]).then((result) => {
-            isDeactivate = result.isDeactivate;
-            chrome.storage.local.set({ isDeactivate: !isDeactivate }).then(() => {
-            });
-            chrome.tabs.sendMessage(tab.id, { greeting: "isDeactivate", data: !isDeactivate })
-        });
-    } else if (command == "invisible") {
-        chrome.storage.local.get(["invisible"]).then((result) => {
-            invisibleee = result.invisible;
-            chrome.storage.local.set({ invisible: !invisibleee }).then(() => {
-            });
-            chrome.tabs.sendMessage(tab.id, { greeting: "ChangeSTATE", data: { sleeping: null, invisible: !invisibleee } })
         });
     }
 });
