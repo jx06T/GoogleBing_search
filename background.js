a1cd43245c99665702d3c242f52fbe1fbf431dbf
jx06T <50313tjx06@gmail.com>
2023-11-26 23:47:47 +0800

    modified:   background.js

--- background.js
+++ background.js
@@ -3,10 +3,11 @@ chrome.commands.onCommand.addListener((command, tab) => {
         chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
             let tab = tabs[0];
             let key = tab.title
-            if (tab.url.includes('https://www.bing.com/search')) {
-                key = key.slice(0, key.length - 5)
-                chrome.tabs.create({ url: 'https://www.google.com.tw/search?q=' + key });
-            } else if (tab.url.includes('https://www.google.com.tw/search')){
+            if (tab.url.includes('https://www.bing.com')) {
+                // key = key.slice(0, key.length - 5)
+                key = key.split(" - ")[0]
+                chrome.tabs.create({ url: 'https://www.google.com/search?q=' + key });
+            } else if (tab.url.includes('https://www.google.com/search')){
                 key = key.slice(0, key.length -11)
                 chrome.tabs.create({ url: 'https://www.bing.com/search?q=' + key });
             }
